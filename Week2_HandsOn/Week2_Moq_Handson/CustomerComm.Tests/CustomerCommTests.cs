dac4b2f41b013fd44ea7aad61213ddf041fe24df
RAJATESH PAUL <rajateshpaul@gmail.com>
2025-06-29 16:24:08 +0530

    Updated Week2_HandsOn with Week2_Moq content

---
+++ Week2_HandsOn/Week2_Moq_Handson/CustomerComm.Tests/CustomerCommTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using Moq;
+using CustomerCommLib;
+using NUnit.Framework.Legacy;
+
+[TestFixture]
+public class CustomerCommTests
+{
+    private Mock<IMailSender> _mailSenderMock;
+    private CustomerComm _customerComm;
+
+    [OneTimeSetUp]
+    public void Init()
+    {
+        _mailSenderMock = new Mock<IMailSender>();
+        _mailSenderMock.Setup(m => m.SendMail(It.IsAny<string>(), It.IsAny<string>())).Returns(true);
+        _customerComm = new CustomerComm(_mailSenderMock.Object);
+    }
+
+    [Test]
+    public void SendMailToCustomer_ReturnsTrue()
+    {
+        var result = _customerComm.SendMailToCustomer();
+        ClassicAssert.IsTrue(result);
+    }
+}
---
+++ Week2_HandsOn/Week2_Moq_Handson/MagicFilesLib/DirectoryExplorer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+public interface IDirectoryExplorer
+{
+    ICollection<string> GetFiles(string path);
+}
+
+namespace MagicFilesLib
+{
+    public class DirectoryExplorer : IDirectoryExplorer
+    {
+        public ICollection<string> GetFiles(string path)
+        {
+            string[] files = Directory.GetFiles(path);
+            return files;
+        }
+    }
+}
+
